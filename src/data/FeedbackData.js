be3922963d6bd3e139a85701a603f63ba3dd4cac
Ike Odira <odiraike8@gmail.com>
2022-08-01 14:13:37 +0100

    first commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id:1,
+        rating:10,
+        text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus ratione temporibus, voluptatibus laudantium quidem rerum. Neque nostrum perspiciatis amet eum.'
+    },
+    {
+        id:2,
+        rating:9,
+        text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus ratione temporibus, voluptatibus laudantium quidem rerum. Neque nostrum perspiciatis amet eum.'
+    },
+    {
+        id:3,
+        rating:7,
+        text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus ratione temporibus, voluptatibus laudantium quidem rerum. Neque nostrum perspiciatis amet eum.'
+    }
+]
+
+export default FeedbackData;
